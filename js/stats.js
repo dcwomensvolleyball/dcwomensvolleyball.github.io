10dd4f9068af101706756ddbe6a8492cec803932
Your Name <you@example.com>
2024-03-12 19:04:29 -0500

    new dates and rules format

--- js/stats.js
+++ js/stats.js
@@ -20,3 +20,19 @@ function showImage(imageId, otherIds) {
   document.getElementById(imageId).style.display = "block";
 }
 
+function collapseTab() {
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+  
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight){
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      } 
+    });
+  }
+}
